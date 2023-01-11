dbc4cd6d35b8915ebd546acd090c25702d4ded81
Your Name <sharmanoopur2510@gmail.com>
2023-01-11 22:13:39 +0530

    changes

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,22 +1,11 @@
 import bg from '../assets/bg.jpg';
-import chef from '../assets/chef.png';
 import B from '../assets/B.png';
-
 import knife from '../assets/knife.png';
 import logo from '../assets/logo.png';
 import menu from '../assets/menu.jpg';
 import overlaybg from '../assets/overlaybg.png';
 import spoon from '../assets/spoon.svg';
 import welcome from '../assets/welcome.jpg';
-import findus from '../assets/findus.png';
-import laurels from '../assets/laurels.png';
-import award01 from '../assets/award01.png';
-import award02 from '../assets/award02.png';
-import award03 from '../assets/award03.png';
-import award05 from '../assets/award05.png';
-import sign from '../assets/sign.png';
-import quote from '../assets/quote.png';
-
 import logonav from '../assets/logonav.png';
 import brand2 from '../assets/brand2.png';
 import one from '../assets/1.jpg';
@@ -34,24 +23,13 @@ import tbl from '../assets/tbl.png';
 
 export default {
   bg,
-  chef,
   B,
-  
   knife,
   logo,
   menu,
   overlaybg,
   spoon,
   welcome,
-  findus,
-  laurels,
-  award01,
-  award02,
-  award03,
-  award05,
-  sign,
-  quote,
- 
   logonav,
   brand2,
   one, two, three, four, five, six, seven, eight, nine, ten,find,tbl,
